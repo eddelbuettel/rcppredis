f16a94c170ac1e5a81d657955ad045eeed9e07b9
Wush Wu <wush@bridgewell.com>
2013-07-19 09:59:46 +0800

    parse redisReply. Need bit64

--- src/redisCommand.cpp
+++ src/redisCommand.cpp
@@ -1,3 +1,4 @@
+#include <boost/shared_ptr.hpp>
 #include <Rcpp.h>
 #include "hiredis/hiredis.h"
 #include "util.hpp"
@@ -15,9 +16,40 @@ using namespace Rcpp;
 SEXP redisCommand(std::string cmd, SEXP Rc) {
 	BEGIN_RCPP
 	redisContext* c(extract_ptr<RredisContext>(Rc)->get_ptr());
-	redisReply* r = (redisReply*) redisCommand(c, cmd.c_str());
-	if (r == NULL) {
+	boost::shared_ptr<redisReply> r((redisReply*) redisCommand(c, cmd.c_str()), std::ptr_fun<void*, void>(freeReplyObject));
+	if (r.get() == NULL) {
 		throw std::runtime_error(std::string(c->errstr));
+	}
+	switch(r->type) {
+	case REDIS_REPLY_STRING: 
+	case REDIS_REPLY_STATUS: {
+		std::string retval(r->str, r->len);
+		return wrap(retval);
+	}
+	case REDIS_REPLY_ERROR: {
+		std::string retval(r->str, r->len);
+		throw std::runtime_error(retval.c_str());
+	}
+	case REDIS_REPLY_INTEGER: {
+		if (r->integer > INT_MAX) {
+			NumericVector retval(1);
+			memcpy(&retval[0], &(r->integer), sizeof(double));
+			retval.attr("class") = wrap("integer64");
+			return retval;
+		}
+		else {
+			int retval = r->integer;
+			return wrap(retval);
+		}
+	}
+	case REDIS_REPLY_NIL: {
+		return R_NilValue;
+	}
+	case REDIS_REPLY_ARRAY: {
+		return wrap("Array returned. TODO!");
+	}
+	default:
+		throw std::logic_error("Unknown type");
 	}
 	END_RCPP
 }
