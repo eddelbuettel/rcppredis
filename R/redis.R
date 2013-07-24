3890b60633586ee47b2f71c8a0f3c5d4ace1def5
Dirk Eddelbuettel <edd@debian.org>
2013-07-23 20:39:20 -0500

    further simplified

--- R/redis.R
+++ R/redis.R
@@ -1,7 +1,4 @@
 ## minimal R interface to redis using the hiredis library
-redis <- function(cmd) {
-    ## some sanity checking on cmd would be nice too...
-    .Call("execRedisCommand", cmd, package="Rhiredis")
-}
 
-loadModule("Redis", TRUE)               # ensure module gets loaded
+## ensure module gets loaded
+loadModule("Redis", TRUE)
