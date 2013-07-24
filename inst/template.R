2873e9ab7b810f0817895d32eda5b0765eb00133
Dirk Eddelbuettel <edd@debian.org>
2013-07-23 20:17:19 -0500

    delete generator files no longer needed

--- inst/template.R
+++
@@ -1,7 +0,0 @@
-#'@title %%summary%%
-#'@export
-redis%%funname%% <- function(%%args-argv%%Rc = NULL) {
-	if (is.null(Rc)) Rc <- getOption("Rhiredis.connect")
-	cmd <- "%%name%%"
-	redisCommand(Rc, cmd%%args-value%%)
-}
