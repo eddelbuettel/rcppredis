e1993ca0165ecf9fed83c3f703b2e365de2118ae
Dirk Eddelbuettel <edd@debian.org>
2013-08-04 20:12:21 -0500

    added serialization / unserialization of an R object

--- demo/simpleRedisClient.R
+++ demo/simpleRedisClient.R
@@ -3,10 +3,18 @@
 suppressMessages(library(rhiredis))
 print(Redis)
 
+## simple ping / pong test
 redis <- new(Redis)
 redis$exec("PING")
 redis$exec("PING")
 
+## set and retrieve a number
 redis2 <- new(Redis, "127.0.0.1", 6379)
 redis2$exec("SET testchannel 42")
 redis2$exec("GET testchannel")
+
+## set and retrieve a complete R object
+data(trees)
+fit <- lm(log(Volume) ~ log(Girth) + log(Height), data=trees)
+redis$exec(paste("SET fits ", rawToChar(serialize(fit,NULL,ascii=TRUE)), sep=""))
+all.equal(unserialize(charToRaw(redis$exec("GET fits"))), fit)
