1106ef0677873edc123f03b75feac6b3e081e376
Dirk Eddelbuettel <edd@debian.org>
2014-05-07 20:33:17 -0500

    expanded new demo

--- demo/simDemo.R
+++ demo/simDemo.R
@@ -6,7 +6,7 @@ library(rbenchmark)
 
 set.seed(123)
 
-N <- 1500
+N <- 2500
 
 x <- xts(100*cumprod(1+rnorm(N)*0.005 + (runif(N)>0.95)*rnorm(N)*0.025),
          order.by=Sys.time()+cumsum(exp(3*runif(N))))
@@ -17,9 +17,12 @@ redis <- new(Redis)
 
 dat <- data.frame(key=as.numeric(index(x)), val=coredata(x))
 
-## insertion is row by row
-for (i in 1:N) {
-    redisZAdd("ex:ascii:series", dat[i,1], dat[i,])
+setAsAscii <- function(dat) {
+    N <- nrow(dat)
+    ## insertion is row by row
+    for (i in 1:N) {
+        redisZAdd("ex:ascii:series", dat[i,1], dat[i,])
+    }
 }
 
 ## retrieval is by list
@@ -28,10 +31,8 @@ getFromAscii <- function() {
     xt <- xts(xx[,-1], order.by=as.POSIXct(xx[,1], origin="1970-01-01"))
 }
 
-mat <- as.matrix(dat)
-## insertion is still row by row
-for (i in 1:N) {
-    redis$zadd("ex:bin:series", mat[i,])
+setAsBinary <- function(dat) {
+    redis$zadd("ex:bin:series", as.matrix(dat))
 }
 
 getFromBinary <- function() {
@@ -39,7 +40,23 @@ getFromBinary <- function() {
     zt <- xts(zz[,-1], order.by=as.POSIXct(zz[,1], origin="1970-01-01"))
 }
 
+redis$zremrangebyscore("ex:ascii:series", 0, Inf)
+redis$zremrangebyscore("ex:bin:series", 0, Inf)
+
+cat("Writing ascii ... patience\n")
+setAsAscii(dat)
+setAsBinary(dat)
+
+xt <- getFromAscii()
+zt <- getFromBinary()
 identical(xt,zt)
+
+redis$zremrangebyscore("ex:ascii:series", 0, Inf)
+redis$zremrangebyscore("ex:bin:series", 0, Inf)
+
+benchmark(setAsAscii(dat), setAsBinary(dat), 
+          replications=1, order="relative")[,1:4]
 
 
 benchmark(getFromAscii(), getFromBinary(), replications=10, order="relative")[,1:4]
+
