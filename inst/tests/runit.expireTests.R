e62a51ba42fbec0f756ff138362b493e3a493358
drknexus <russell.s.pierce@gmail.com>
2015-11-22 10:07:28 +0000

    tests use liberal timings because Sys.sleep does not guarantee sleep for the full interval

--- inst/tests/runit.expireTests.R
+++ inst/tests/runit.expireTests.R
@@ -4,7 +4,7 @@ test_01_setup <- function() {
     key <<- "RcppRedis:test:myexpires"
 }
 
-test_02_keyexpiresAfter1 <- function() {
+test_02_keyexpiresAfter1s <- function() {
     redis$set(key, 10)
     redis$expire(key, 1)
     Sys.sleep(1.5)
@@ -12,7 +12,7 @@ test_02_keyexpiresAfter1 <- function() {
     checkEquals(n, 0)
 }
 
-test_02_keyexpiresAfter2 <- function() {
+test_02_keyexpiresAfter2s <- function() {
     redis$set(key, 10)
     redis$expire(key, 2)
     Sys.sleep(1)
@@ -23,6 +23,16 @@ test_02_keyexpiresAfter2 <- function() {
     checkEquals(n, 0)
 }
 
+test_02_keyexpiresAfter1500ms <- function() {
+    redis$set(key, 10)
+    redis$expire(key, .9)
+    Sys.sleep(.3)
+    n <- redis$exists(key)
+    checkEquals(n, 1)
+    Sys.sleep(1.6)    
+    n <- redis$exists(key)
+    checkEquals(n, 0)
+}
 
 test_03_cleanup <- function() {
     ## delete key
