e57b60b85f7e6f84d277ade6b3a704ba78cc1c99
Dirk Eddelbuettel <edd@debian.org>
2022-01-28 15:59:29 -0600

    generalize pruner

--- inst/pubsub/pruneRedis.r
+++ inst/pubsub/pruneRedis.r
@@ -7,17 +7,20 @@ suppressMessages({
 })
 
 defaultTZ <- "America/Chicago"
-symbol <- "ES=F"
 host <- "localhost"
+symbols <- c("BTC=F", "ES=F", "GC=F", "CL=F")
 
 redis <- new(Redis, host)
 if (redis$ping() != "PONG") stop("No Redis server?", call. = FALSE)
-m <- redis$zrange(symbol, 0, -1)
-colnames(m) <- c("Time", "Close", "Change", "PctChange", "Volume")
-y <- xts(m[,-1], order.by=anytime(as.numeric(m[,1])))
-if (as.numeric(index(y)[1], Sys.time(), units="days") > 30) {
-    onemonthago <- Sys.time() - 60*60*24*31
-    cutoff <- as.numeric(onemonthago)
-    lastold <- max(which(m[, 1] < cutoff ))
-    redis$zremrangebyscore(symbol, 0, m[lastold,1])
+
+for (symbol in symbols) {
+    m <- redis$zrange(symbol, 0, -1)
+    colnames(m) <- c("Time", "Close", "Change", "PctChange", "Volume")
+    y <- xts(m[,-1], order.by=anytime(as.numeric(m[,1])))
+    if (as.numeric(index(y)[1], Sys.time(), units="days") > 30) {
+        onemonthago <- Sys.time() - 60*60*24*31
+        cutoff <- as.numeric(onemonthago)
+        lastold <- max(which(m[, 1] < cutoff ))
+        redis$zremrangebyscore(symbol, 0, m[lastold,1])
+    }
 }
