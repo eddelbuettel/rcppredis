c43c52038a615f7928e98084ce0cf79ec1333a5f
Dirk Eddelbuettel <edd@debian.org>
2022-01-30 18:16:20 -0600

    update Python examples

--- inst/examples/readPython.py
+++ inst/examples/readPython.py
@@ -2,17 +2,17 @@
 
 import redis
 
-redishost  = "localhost"
+redishost = "localhost"
 redisserver = redis.StrictRedis(redishost)
 
 key1 = "ex:ascii:simpleString"
 val1 = redisserver.get(key1)
-print "Got", val1, "from", key1
+print("Got", val1, "from", key1)
 
 key2 = "ex:ascii:scalarVal"
 val2 = redisserver.lpop(key2)
-print "LPop'ed", val2, "from", key2
+print("LPop'ed", val2, "from", key2)
 
 key3 = "ex:ascii:vectorVal"
 val3 = redisserver.lpop(key3)
-print "LPop'ed", val3, "from", key3
+print("LPop'ed", val3, "from", key3)
--- inst/examples/writePython.py
+++ inst/examples/writePython.py
@@ -2,20 +2,23 @@
 
 import redis
 
-redishost  = "localhost"
+redishost = "localhost"
 redisserver = redis.StrictRedis(redishost)
 
 key1 = "ex:ascii:simpleString"
 val1 = "abracadabra"
 res = redisserver.set(key1, val1)
-print "Set", val1, "under", key1, "with result", res
+print("Set", val1, "under", key1, "with result", res)
 
 key2 = "ex:ascii:scalarVal"
 val2 = 42
 res = redisserver.rpush(key2, val2)
-print "Pushed", val2, "under", key2, "with result", res
+print("Pushed", val2, "under", key2, "with result", res)
 
 key3 = "ex:ascii:vectorVal"
-val3 = [40, 41, 42]
+#val3 = [40, 41, 42]
+val3 = "40, 41, 42"
 res = redisserver.rpush(key3, val3)
-print "Pushed", val3, "under", key3, "with result", res
+print("Pushed", val3, "under", key3, "with result", res)
+
+print("Done.")
